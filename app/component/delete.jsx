9c7a4c9458f7a7f281db852bd29c55e99b46a1a3
pramodahetti <pramodahetti05@gmail.com>
2025-07-13 12:43:18 +0530

    removing build errors

--- app/component/delete.jsx
+++ app/component/delete.jsx
@@ -1,23 +1,14 @@
 'use client'
 import { Trash } from "lucide-react";
 import React, { useState } from "react";
-import { deletePost } from "../session"; 
 import {Toaster,toast} from 'sonner';
 
 export default function Delete({id}){
-   async function click(){
-      try{
-         id = parseInt(id, 10); 
-          await deletePost(id);
-          toast.success('post deleted successfully')
-      }catch(error){
-         toast.error('error')
-      }
-   }
+
  return (
    <>
    <Toaster theme="dark" richColors position="top"/>
-   <Trash onClick={click}  className="text-base pt-2 text-red-600 cursor-pointer" /> 
+   <Trash  className="text-base pt-2 text-red-600 cursor-pointer" /> 
    </>
  )
 }
